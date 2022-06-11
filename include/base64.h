4e0138c5ed2fe3f4bd4006cfbf4a22d160ff2fe7
mitander <carlmitander@gmail.com>
2022-06-11 16:18:10 +0200

    Add base64 encoding and decoding

--- include/base64.h
+++ include/base64.h
@@ -6,4 +6,4 @@
 unsigned char *base64_encode(const unsigned char *src, size_t len, size_t *out_len);
 unsigned char *base64_decode(const unsigned char *src, size_t len, size_t *out_len);
 
-#endif // BASE64_H
+#endif // TWS_BASE64_H
